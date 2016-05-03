9d8b805a2cfe715954ed034ca2939bd90f6190bc
Dennis Dao <dennisdao@outlook.com>
2016-05-03 16:08:13 +1000

    Add unit test.cs

--- C#/Test.cs
+++ C#/Test.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-
+using NUnit.Framework;
 namespace MyGame
-{
+{   
+	[TestFixture ()]
 	public class Test
 	{
-		public Test ()
+		[Test ()]
+			public void TestNoShip ()
+			{
+				BattleShipsGame Game = new BattleShipsGame ();
+				Player Player = new Player (Game);
+
+				AttackResult result = Player.PlayerGrid.HitTile (1, 1);
+				Assert.AreEqual ("missed", result.Text, "should return miss becuase there is no ship at that cordinates");
+			}
+
+		[Test ()]
+		public void TestHitShip ()
 		{
+			BattleShipsGame Game = new BattleShipsGame ();
+			Player Player = new Player (Game);
+
+			Player.PlayerGrid.MoveShip (1, 1, ShipName.Submarine, Direction.UpDown);
+
+			AttackResult result = Player.PlayerGrid.HitTile (1, 1);
+			Assert.AreEqual ("hit something!", result.Text, "should hit the shit because the subarmine is in the cordinates");
 		}
+
 	}
 }
 
